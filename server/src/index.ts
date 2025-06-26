3394c970b043ec97903192278d2035ef121804cf
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-06-26 22:13:05 +0800

    feat: implement matchmaking logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,19 +4,16 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
 
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import jwt from "jsonwebtoken";
+
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -29,15 +26,13 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
-    credentials: true,
   },
 });
 
-// middleware
 io.use((socket, next) => {
   const token = socket.handshake.auth.token;
 
-  console.log("Token from client:", token);
+  // console.log("Token from client:", token);
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
@@ -49,13 +44,51 @@ io.use((socket, next) => {
   }
 });
 
-// Socket connection
+const queue: Socket[] = [];
+
 io.on("connection", (socket) => {
-  const user = socket.data.user;
-  console.log("✅ Connected:", socket.id, user.email);
+  console.log(`User: ${socket.id} connected`);
+
+  socket.on("find_match", () => {
+    if (queue.includes(socket)) {
+      return;
+    }
+
+    if (queue.length === 0) {
+      // No partner yet, enqueue self
+      queue.push(socket);
+      socket.emit("waiting");
+      console.log(`${socket.id} is waiting`);
+    } else {
+      const partner = queue.shift()!;
+
+      const roomId = uuidv4();
+
+      socket.join(roomId);
+      partner.join(roomId);
+
+      socket.data.roomId = roomId;
+      socket.data.roomId = roomId;
+
+      socket.emit("matched", { roomId });
+      partner.emit("matched", { roomId });
+
+      console.log(`Matched ${socket.id} with ${partner.id} in ${roomId}`);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const index = queue.indexOf(socket);
+
+    if (index !== -1) {
+      queue.splice(index, 1);
+    }
+
+    console.log(`Disconnected: ${socket.id}`);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${3000}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
