7866e5cc7c550221531d0341fefb9dd9c1235863
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-07-04 23:33:01 +0800

    feat: update chat bubble styles for improved theming and responsiveness

--- client/src/components/chat-bubble.tsx
+++ client/src/components/chat-bubble.tsx
@@ -3,6 +3,7 @@ import type { Message } from "@/types/message";
 
 export default function ChatBubble({ message }: { message: Message }) {
   const isMe = message.from === socket.id;
+
   return (
     <div
       className={`flex transition-all duration-200 ease-in-out ${
@@ -10,7 +11,11 @@ export default function ChatBubble({ message }: { message: Message }) {
       }`}
     >
       <div
-        className={`max-w-96 transform rounded-2xl px-4 py-2 text-sm break-words shadow ${isMe ? "rounded-br-none bg-blue-500 text-white" : "rounded-tl-none bg-gray-200 text-black"} scale-100 opacity-100 transition-all duration-200 ease-in-out`}
+        className={`max-w-4/5 transform rounded-2xl px-4 py-2 text-sm break-words shadow ${
+          isMe
+            ? "bg-primary text-primary-foreground rounded-br-none"
+            : "bg-muted text-muted-foreground rounded-tl-none"
+        } scale-100 opacity-100 transition-all duration-200 ease-in-out`}
       >
         {message.message}
       </div>
