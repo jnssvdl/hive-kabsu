88da37791d39a26aaa571818d6f36d5c6319c53a
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-10-31 23:01:13 +0800

    fix bug on disconnected

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,36 +1,31 @@
 import ChatForm from "@/components/chat-form";
 import { useChat } from "@/components/chat-context";
 import ChatBox from "@/components/chat-box";
-import { Button } from "@/components/ui/button";
-import { Loader2, MessageCircle, Users } from "lucide-react";
+import { Loader2, Unlink, Users } from "lucide-react";
 import { useEffect, useRef } from "react";
 import TypingIndicator from "@/components/typing-indicator";
 import Header from "@/components/header";
 import StatusText from "@/components/status-text";
+import NewChat from "@/components/new-chat";
 
 export default function Chat() {
-  const { status, isTyping, messages, findMatch } = useChat();
+  const { status, isTyping, messages } = useChat();
 
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    ref.current?.scrollIntoView({ behavior: "smooth" });
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
   }, [messages, status]);
 
   return (
-    <div className="flex h-screen justify-center lg:p-4">
-      <div className="flex w-full max-w-5xl flex-col border shadow">
+    <div className="mx-auto flex h-screen max-w-5xl flex-col md:p-4">
+      <div className="bg-card/20 flex flex-1 flex-col overflow-hidden border md:rounded-2xl">
         <Header />
 
-        <div className="flex-1 overflow-y-auto">
-          <div className="flex min-h-full flex-col justify-end gap-2 p-4">
-            {status === "matched" && (
-              <StatusText
-                Icon={Users}
-                text="You're connected! Say hi to them!"
-              />
-            )}
-
+        <div className="flex-1 overflow-y-auto p-4" ref={ref}>
+          <div className="flex min-h-full flex-col justify-end gap-2">
             {status === "waiting" && (
               <StatusText
                 Icon={Loader2}
@@ -39,32 +34,38 @@ export default function Chat() {
               />
             )}
 
-            <ChatBox messages={messages} />
+            {status === "matched" && (
+              <StatusText
+                Icon={Users}
+                text="You're connected! Say hi to them!"
+              />
+            )}
 
+            <ChatBox messages={messages} />
             {isTyping && <TypingIndicator />}
 
             {status === "disconnected" && (
-              <StatusText
-                Icon={Loader2}
-                text="Waiting someone you can chat with..."
-                className="text-destructive"
-              />
+              <>
+                <StatusText
+                  Icon={Unlink}
+                  text="They have disconnected!"
+                  className="text-destructive"
+                />
+                <div className="flex justify-center">
+                  <NewChat />
+                </div>
+              </>
             )}
 
             {status === "idle" && (
               <div className="flex justify-center">
-                <Button onClick={findMatch} className="rounded-full">
-                  <MessageCircle />
-                  <span>New chat</span>
-                </Button>
+                <NewChat />
               </div>
             )}
-
-            <div ref={ref}></div>
           </div>
         </div>
 
-        <div className="bg-background sticky bottom-0 border-t p-4">
+        <div className="bg-background border-t p-4">
           <ChatForm />
         </div>
       </div>
