f8f9727f3e803997ca02b780346bff3216d9292f
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-11-30 01:28:52 +0800

    refactor: load service account key from environment variable instead of file

--- server/src/lib/firebase-admin.ts
+++ server/src/lib/firebase-admin.ts
@@ -1,6 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../../serviceAccountKey.json";
+// import serviceAccount from "../../serviceAccountKey.json";
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.SERVICE_ACCOUNT_KEY!, "base64").toString("utf-8")
+);
 
 initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
