37fd2158a22e2263ed2ee39eee6f82d475265dff
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-11-29 19:25:31 +0800

    add contact form

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,20 +1,16 @@
-import { useAuth } from "@/components/auth-context";
 import { GoogleLogin } from "../components/google-login";
-import { useNavigate } from "react-router-dom";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 import { DotPattern } from "@/components/ui/dot-pattern";
 import { cn } from "@/lib/utils";
 import { FaShieldAlt, FaUserSecret } from "react-icons/fa";
 import { IoLogoFirebase } from "react-icons/io5";
 import Feature from "@/components/feature";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
-  const { isAuthenticated } = useAuth();
-
-  const navigate = useNavigate();
-
   return (
     <>
       {/* header */}
@@ -28,8 +24,9 @@ export default function Home() {
       {/* hero or login */}
       <div className="from-primary-foreground/60 to-background relative flex min-h-screen items-center justify-center overflow-hidden bg-gradient-to-b">
         <DotPattern
+          // glow={true}
           className={cn(
-            "[mask-image:radial-gradient(200px_circle_at_center,white,transparent)]",
+            "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
           )}
         />
 
@@ -42,29 +39,19 @@ export default function Home() {
             time. No profile, just pure conversation.
           </p>
 
-          {isAuthenticated ? (
-            <Button
-              size={"lg"}
-              className="group rounded-full"
-              onClick={() => navigate("/chat")}
-            >
-              Start chatting
-              <ArrowRight className="transform transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
-            </Button>
-          ) : (
-            <div className="flex flex-col gap-y-4">
-              <div className="mx-auto">
-                <GoogleLogin />
-              </div>
-              <p className="text-muted-foreground text-center text-sm">
-                Log in with your CvSU account to start chatting anonymously.
-              </p>
+          <div className="flex flex-col gap-y-4">
+            <div className="mx-auto">
+              <GoogleLogin />
             </div>
-          )}
+            <p className="text-muted-foreground text-center text-sm">
+              Log in with your CvSU account to start chatting anonymously.
+            </p>
+          </div>
         </div>
       </div>
 
       {/* Some other stuff */}
+      {/* features */}
       <div className="from-primary-foreground/60 to-background border-t-2 bg-gradient-to-t py-24">
         <div className="container mx-auto max-w-screen-xl px-4">
           <h2 className="mb-12 text-center text-3xl font-semibold text-balance">
@@ -77,7 +64,50 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className="from-primary-foreground/60 to-background border-t-2 bg-gradient-to-b py-8">
+
+      {/* contact */}
+      <div className="from-primary-foreground/60 to-background border-t-2 bg-gradient-to-b py-24">
+        <div className="container mx-auto max-w-screen-md px-4">
+          <h2 className="mb-6 text-center text-3xl font-semibold text-balance">
+            Contact Us
+          </h2>
+          <p className="text-muted-foreground mb-6 text-center">
+            We'd love to hear from you! Whether you have a question, feedback,
+            or just want to say hello, send us a message!
+          </p>
+
+          <form className="bg-card/40 flex flex-col gap-6 rounded-lg border p-6 shadow-sm">
+            <div className="flex flex-col gap-4 md:flex-row">
+              <div className="flex flex-1 flex-col gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Your name" />
+              </div>
+              <div className="flex flex-1 flex-col gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="you@example.com" />
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="message">Message</Label>
+              <Textarea
+                id="message"
+                placeholder="Your message"
+                className="field-sizing-fixed min-h-0"
+                rows={4}
+              />
+            </div>
+
+            <div className="flex justify-end">
+              <Button type="submit" className="w-full md:w-auto">
+                Send
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <footer className="from-primary-foreground/60 to-background border-t-2 bg-gradient-to-t py-8">
         <div className="container mx-auto text-center">
           <p className="text-sm">
             &copy; {new Date().getFullYear()} chat.kabsu
