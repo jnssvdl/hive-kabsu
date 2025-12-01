78e6c1e501f2b2afacdabc917c9b71e40fe1c286
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-12-01 20:57:45 +0800

    change background color of typing indicator to match chat bubble

--- client/src/components/typing-indicator.tsx
+++ client/src/components/typing-indicator.tsx
@@ -1,9 +1,9 @@
 export default function TypingIndicator() {
   return (
-    <div className="bg-muted inline-flex max-w-fit items-center space-x-1 rounded-full p-4">
-      <span className="bg-muted-foreground h-2 w-2 animate-bounce rounded-full" />
-      <span className="bg-muted-foreground h-2 w-2 animate-bounce rounded-full" />
-      <span className="bg-muted-foreground h-2 w-2 animate-bounce rounded-full" />
+    <div className="bg-card inline-flex max-w-fit items-center space-x-1 rounded-full p-4">
+      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
+      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
+      <span className="bg-card-foreground h-2 w-2 animate-bounce rounded-full" />
     </div>
   );
 }
