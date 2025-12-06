5d06e7b12e56c98b8860426caf53f865a6db02d7
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-12-06 12:53:18 +0800

    add tailwind animate classes

--- client/src/components/chat-bubble.tsx
+++ client/src/components/chat-bubble.tsx
@@ -6,16 +6,18 @@ export default function ChatBubble({ message }: { message: Message }) {
 
   return (
     <div
-      className={`flex transition-all duration-200 ease-in-out ${
-        isMe ? "justify-end" : "justify-start"
+      className={`animate-in flex duration-200 ${
+        isMe
+          ? "slide-in-from-bottom-20 justify-end"
+          : "slide-in-from-top-20 justify-start"
       }`}
     >
       <div
-        className={`max-w-4/5 transform rounded-2xl px-4 py-2 break-words ${
+        className={`max-w-[80%] rounded-2xl px-4 py-2 break-words ${
           isMe
             ? "bg-primary text-primary-foreground rounded-br-none"
             : "bg-accent text-accent-foreground rounded-tl-none"
-        } scale-100 opacity-100 transition-all duration-200 ease-in-out`}
+        }`}
       >
         {message.message}
       </div>
--- client/src/components/typing-indicator.tsx
+++ client/src/components/typing-indicator.tsx
@@ -1,6 +1,6 @@
 export default function TypingIndicator() {
   return (
-    <div className="bg-accent inline-flex max-w-fit items-center space-x-1 rounded-full p-4">
+    <div className="bg-accent animate-in slide-in-from-bottom-20 inline-flex max-w-fit items-center space-x-1 rounded-full p-4 duration-200">
       <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
       <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
       <span className="bg-accent-foreground h-2 w-2 animate-bounce rounded-full" />
