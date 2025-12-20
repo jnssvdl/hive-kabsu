d3996aba55f57c52a505bead6d4b8ac8ab59e5e9
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-12-20 12:31:06 +0800

    make the new chat button accept a prop for button variant

--- client/src/components/new-chat.tsx
+++ client/src/components/new-chat.tsx
@@ -2,11 +2,23 @@ import { useChat } from "./chat-context";
 import { MessageCircle } from "lucide-react";
 import { Button } from "./ui/button";
 
-export default function NewChat() {
+type NewChatVariant = {
+  variant:
+    | "link"
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | null
+    | undefined;
+};
+
+export default function NewChat({ variant }: NewChatVariant) {
   const { findMatch } = useChat();
 
   return (
-    <Button onClick={findMatch} className="rounded-full">
+    <Button onClick={findMatch} className="rounded-full" variant={variant}>
       <MessageCircle />
       New chat
     </Button>
